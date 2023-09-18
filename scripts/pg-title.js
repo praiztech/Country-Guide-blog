2793c0a5e80a6b9f7d03f987d6fa82171599956b
praiztech <praisefagbabiaka@gmail.com>
2023-09-18 13:16:59 +0100

    modified page title decision tree

--- scripts/pg-title.js
+++ scripts/pg-title.js
@@ -1,24 +1,25 @@
 "use strict";
 (function() {
-  const page = decodeURIComponent(location.hash.slice(1));
-  if (page === '') {
-    document.title = 'Where in the World?';
-  } else {
-    switch (location.pathname) { //parent page
-      case '/404.html':
-        document.title = 'Page Not Found - Where in the World?';
-        break;
-      case '/details.html':
-        const prefix = (
-          page.split(' ').map(
-            (word) => ['and', 'of', 'the'].includes(word) ? word : `${word[0].toUpperCase() + word.slice(1)}`
-            ).join(' ')
-        );
-        document.title = `${prefix} - Where in the World?`;
-        break;
-      default: //index.html
-        document.title = `Page ${page.slice(4)} - Where in the World?`;
-        break;
+  try {
+    const page = decodeURIComponent(location.hash.slice(1));
+    if (page === '') {
+      document.title = 'Where in the World?';
+    } else {
+      switch (location.pathname) { //parent page
+        case '/details.html':
+          const prefix = (
+            page.split(' ').map(
+              (word) => ['and', 'of', 'the'].includes(word) ? word : `${word[0].toUpperCase() + word.slice(1)}`
+              ).join(' ')
+          );
+          document.title = `${prefix} - Where in the World?`;
+          break;
+        default: //index.html
+          document.title = `Page ${page.slice(4)} - Where in the World?`;
+          break;
+      }
     }
+  } catch (error) {
+    document.title = 'Where in the World?';
   }
 })();
